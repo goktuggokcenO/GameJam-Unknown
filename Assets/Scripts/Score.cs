3674c56d1d399ae64cfd7d38c5203758ab6c03ef
elifnazcelik <d.elifcelik@gmail.com>
2023-12-09 03:16:04 +0300

    Add score scrıpth

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int scoreValue = 0;
+
+    Text score;
+
+    void Statrt()
+    {
+        score = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        score.text = "Score: "+ scoreValue;
+    }
+
+}
