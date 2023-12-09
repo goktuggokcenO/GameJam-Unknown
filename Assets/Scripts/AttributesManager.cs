008e4742a18f8d966d7bd251f9da740c8c9a242c
elifnazcelik <d.elifcelik@gmail.com>
2023-12-09 10:27:39 +0300

    health bar updated

--- Assets/Scripts/AttributesManager.cs
+++ Assets/Scripts/AttributesManager.cs
@@ -1,28 +1,46 @@
-// Libraries and files
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-// Character atributes
 public class AttributesManager : MonoBehaviour
 {
-    // Veriables
     public int health;
     public int attack;
+    public HealthBar healthBar;
 
-    // Take damage when hit by a projectile
+    public void Start()
+    {
+        HealthBar healthBar= GetComponentInChildren<HealthBar>();
+        if (healthBar != null)
+        {
+            UpdateHealthBar();
+        }
+    }
     public void TakeDamage(int amount)
     {
         health -= amount;
+        if (healthBar != null)
+        {
+            UpdateHealthBar();
+        }
     }
 
-    // Deal damage when shhot
     public void DealDamage(GameObject target)
     {
         var atm = target.GetComponent<AttributesManager>();
-        if(atm != null)
+        if (atm != null)
         {
             atm.TakeDamage(attack);
         }
+    }
+
+    private void UpdateHealthBar()
+    {
+        // Gerekirse maxHealth ve currentHealth parametreleri hesaplanýr
+        float maxHealth = 100f; // Örnek bir deðer, gerçek deðeri projenize göre ayarlayýn
+        float currentHealth = Mathf.Clamp01((float)health / maxHealth); // 0-1 aralýðýnda bir deðere dönüþtür
+
+        healthBar.UpdateHealthBar(maxHealth, currentHealth);
     }
 }
