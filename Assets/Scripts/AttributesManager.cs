190009bf86541372af5655d71bb6c010eccdec26
Göktuğ Gökcen <goktuggokcen0@gmail.com>
2023-12-09 09:17:35 +0200

    Update Atributes script

--- Assets/Scripts/AttributesManager.cs
+++ Assets/Scripts/AttributesManager.cs
@@ -1,17 +1,22 @@
+// Libraries and files
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Character atributes
 public class AttributesManager : MonoBehaviour
 {
+    // Veriables
     public int health;
     public int attack;
 
+    // Take damage when hit by a projectile
     public void TakeDamage(int amount)
     {
         health -= amount;
     }
 
+    // Deal damage when shhot
     public void DealDamage(GameObject target)
     {
         var atm = target.GetComponent<AttributesManager>();
